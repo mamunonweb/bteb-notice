b300c4cba13e7e416183b26c976700eb8f6b5c35
Mamun <mamunonweb@gmail.com>
2022-01-08 00:10:55 +0600

    Add spinner for scroll bottom

--- src/utils/fetchNotices.js
+++ src/utils/fetchNotices.js
@@ -1,6 +1,7 @@
 import { createSignal, createEffect } from 'solid-js'
 
 export default function fetchNotices() {
+  let empty = false
   const [page, setPage] = createSignal(0)
   const [loading, setLoading] = createSignal(false)
   const [notices, setNotices] = createSignal([])
@@ -10,15 +11,20 @@ export default function fetchNotices() {
     const newNotices = await fetch(
       `https://notice.opps.workers.dev/${page()}`
     ).then(res => res.json())
+
+    if (newNotices.length === 0) {
+      empty = true
+    }
+
     setNotices([...notices(), ...newNotices])
     setLoading(false)
   })
 
-  function nextChunk() {
-    if (loading() === false) {
+  function fetchNextChunk() {
+    if (loading() === false && empty === false) {
       setPage(page() + 1)
     }
   }
 
-  return { notices, loading, nextChunk }
+  return { notices, loading, fetchNextChunk }
 }
