c2f91dcf5b6c9b3746b1eb5a541c9dc161941be5
Mamun <mamunonweb@gmail.com>
2022-01-07 22:14:50 +0600

    Support for theme (dark + whtie)

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ const colors = require('tailwindcss/colors')
 
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: 'class',
   theme: {
     extend: {
       fontFamily: {
