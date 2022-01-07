0a9d2cd0b9883971538c307fb19f5c22d5944501
Mamun <mamunonweb@gmail.com>
2022-01-07 22:53:47 +0600

    Simplify theme mechanism

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -1,80 +1,33 @@
-import { createSignal, createEffect, onCleanup } from 'solid-js'
+import { createSignal, createEffect } from 'solid-js'
 
 import { MoonIcon, SunIcon } from './icons'
+import { getLocaleTheme, setLocaleTheme, setThemeClass } from './theme'
 
 export default function ThemeToggle() {
-  if (['DARK', 'WHITE'].indexOf(localStorage.THEME) < 0) {
-    localStorage.THEME = 'PREFERS_SCHEME'
-  }
+  const [theme, setTheme] = createSignal(getLocaleTheme())
 
-  const mm = window.matchMedia('(prefers-color-scheme: dark)')
-
-  const [theme, setTheme] = createSignal(localStorage.THEME)
-  const [prefersScheme, setPrefersScheme] = createSignal(
-    mm.matches ? 'DARK' : 'WHITE'
-  )
-
-  function handleMedia() {
-    setPrefersScheme(mm.matches ? 'DARK' : 'WHITE')
-  }
-
-  mm.addEventListener('change', handleMedia)
-
-  onCleanup(() => {
-    mm.removeEventListener('change', handleMedia)
-  })
-
-  function handleTheme(theme) {
-    setTheme((localStorage.THEME = theme))
+  function handleTheme() {
+    setTheme(theme() === 'DARK' ? 'WHITE' : 'DARK')
+    setLocaleTheme(theme())
   }
 
   createEffect(() => {
-    setDarkClass(
-      (theme() === 'PREFERS_SCHEME' ? prefersScheme() : theme()) === 'DARK'
-    )
+    setThemeClass(theme() === 'DARK')
   })
-
-  const map = {
-    PREFERS_SCHEME: {
-      title: 'Dark',
-      next: 'DARK',
-      content: <MoonIcon className="m-auto" />,
-    },
-    DARK: {
-      title: 'White',
-      next: 'WHITE',
-      content: <SunIcon className="m-auto" />,
-    },
-    WHITE: {
-      title: 'Auto',
-      next: 'PREFERS_SCHEME',
-      content: (
-        <span className="m-auto inline-block border-[12px] rounded-full border-gray-800 border-b-white border-r-white shadow" />
-      ),
-    },
-  }
-
-  function getMapItem() {
-    return map[theme()]
-  }
 
   return (
     <>
       <button
-        title={getMapItem().title}
-        onClick={() => handleTheme(getMapItem().next)}
+        title={theme() === 'DARK' ? 'Dark' : 'White'}
+        onClick={() => handleTheme()}
         className="h-12 w-12 flex text-gray-500"
       >
-        {getMapItem().content}
+        {theme() === 'DARK' ? (
+          <MoonIcon className="m-auto" />
+        ) : (
+          <SunIcon className="m-auto" />
+        )}
       </button>
     </>
   )
-}
-
-function setDarkClass(isDark) {
-  if (isDark) {
-    document.body.classList.add('dark')
-  } else {
-    document.body.classList.remove('dark')
-  }
 }
