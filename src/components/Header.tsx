a5d48adbb4a7b7a71b41629621593be60d71ce67
Al Mamun <mamunonweb@gmail.com>
2023-03-01 03:57:22 +0600

    refactor: target _blank

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,11 @@ export default function Header() {
   return (
     <header className="fixed w-full bg-white shadow-lg dark:bg-gray-800">
       <div className="max-w-2xl mx-auto h-12 px-2 flex items-center">
-        <a href="https://biit-college.netlify.app">
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://biit-college.netlify.app"
+        >
           <img className="w-8" src="/favicon.svg" alt="BIIT Logo" />
         </a>
         <h1 className="ml-2 text-lg font-medium">BTEB Notice</h1>
